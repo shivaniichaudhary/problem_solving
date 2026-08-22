4e12a36897d0b24dda418c7b0ee2bb50a833f206
shivaniichaudhary <shivanichaudhary300487@gmail.com>
2026-08-22 20:21:13 +0530

    Time: 7 ms (72.55%) | Memory: 49.4 MB (92.50%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedKey = new String(charArray);
+
+            map.putIfAbsent(sortedKey, new ArrayList<>());
+            map.get(sortedKey).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
