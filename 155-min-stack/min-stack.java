9f26b643cae492f528c80ba34636180b26083871
shivaniichaudhary <shivanichaudhary300487@gmail.com>
2026-08-26 20:17:19 +0530

    Time: 33 ms (71.69%) | Memory: 103.1 MB (11.57%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,32 @@
+class MinStack {
+    private Deque<Integer> mainStack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+    
+    public void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
