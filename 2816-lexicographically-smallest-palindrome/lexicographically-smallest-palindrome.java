68a9b64d061f08d0b008ee698632171d00b1019a
shivaniichaudhary <shivanichaudhary300487@gmail.com>
2026-09-26 22:30:24 +0530

    Time: 5 ms (100.00%) | Memory: 47 MB (67.07%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                // Choose the smaller character alphabetically for both positions
+                char minChar = (char) Math.min(arr[left], arr[right]);
+                arr[left] = minChar;
+                arr[right] = minChar;
+            }
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+}
